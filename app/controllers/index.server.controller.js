dbe10d4ed8da0d4670ea43063bb8375376e55f7e
levimatheri <levimatheri@gmail.com>
2020-07-05 12:28:33 -0400

    Initial commit. Add Express and mongoose

---
+++ app/controllers/index.server.controller.js
@@ -0,0 +1,11 @@
+const config = require("../../config/config");
+
+exports.render = function(req, res) {
+    if (req.session.lastVisit) {
+        console.log(req.session.lastVisit);
+    }
+
+    req.session.lastVisit = new Date();
+    
+    res.render('index', {title: 'Hello world'});
+};
